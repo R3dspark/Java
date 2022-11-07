d175cd2e3138487578e5445c3827770a13097485
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2022-11-07 11:33:52 +0200

    chatbot stage 1-st

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,7 @@
+public class ChatBot {
+    public static void main(String[] args){
+        System.out.println("""
+                            Hello my name is Amogus
+                            I was created in 2022""");
+    }
+}
