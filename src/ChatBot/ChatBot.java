c849012efb4c61d1c9c9ee4b84f3cbd325254b45
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2022-11-09 08:27:45 +0200

    chatbot stage 2-nd

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class ChatBot {
     public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
         System.out.println("""
                             Hello my name is Amogus
                             I was created in 2022""");
+        System.out.print("Please, remind me your name: ");
+        String name = in.nextLine();
+        System.out.printf("What a great name you have! %s", name);
+        in.close();
     }
 }
