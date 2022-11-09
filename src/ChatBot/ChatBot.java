c57e5c3d9b33372e97e96c590c99b712ef9b1850
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2022-11-09 09:55:12 +0200

    chatbot stage 3-rd

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -8,6 +8,13 @@ public class ChatBot {
         System.out.print("Please, remind me your name: ");
         String name = in.nextLine();
         System.out.printf("What a great name you have! %s", name);
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7. ");
+        int remainder3 = in.nextInt();
+        int remainder5 = in.nextInt();
+        int remainder7 = in.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        System.out.println("Your age is" + age + "that's a good time to start programming!");
         in.close();
     }
 }
